79ed7d081dc99dc523f9303d66fc36336bde3187
ande3577 <ande3577@gmail.com>
2013-08-30 18:15:18 -0500

    Initial Commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_all_users_custom_field do
+  name 'Redmine All Users Custom Field plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
